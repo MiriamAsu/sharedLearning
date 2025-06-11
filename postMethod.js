a418a28a11997f568b5b37d29f40325dcea627bf
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-06-11 13:52:51 +0100

    new postMethod.js: a script that explain how to use the post method in an async function

---
+++ postMethod.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const postBody = JSON.stringify({
+	title: 'Hello Miriam',
+	body: 'How are you doing today?',
+	userId: 1
+})
+const postEndpoint = 'https://jsonplaceholder.typicode.com/posts'
+const methodType = {
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json',
+	},
+	body: postBody
+}
+
+async function apiTest () {
+	try {
+		const response = await fetch(postEndpoint, methodType);
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+		const data = await response.json();
+		console.log(`API Test Successful with status code of: ${response.status}\n${JSON.stringify(data, null, 4)}`);
+	} catch (error) {
+		console.error('API Test Failed:', error);
+	}
+}
+apiTest()
+
+// methods:
+// 1. GET - fetches data from the server
+// 2. POST - fetch and send data to the server
+// 3. PUT - replaces an existing data on the server
+// 4. DELETE - removes data from the server
+// 5. PATCH - replaces some of the data on the server
+
+
+// response status codes:
+// 1. 100
+// 2. 200, 201, etc - success, the operation was successful
+// 3. 300, 301, 302, etc - redirection, the requested resource has moved
+// 4. 400, 401, 403, etc - client error, the request was invalid or unauthorized
+// 5. 500, 501, 502, etc - server error, the server encountered an error while processing the request
