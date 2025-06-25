151095c4cf144d7989713fccfc9cc3e19a74ea99
coder-guru26 <mimidazzle14@gmail.com>
2025-06-25 13:31:56 +0100

    this is a new file

---
+++ templatelit.js
@@ -0,0 +1,7 @@
+const variable1 = "This is variable1"
+const variable2 = 'This is variable2'
+const variable3 = `This is variable3 ${variable1} - ${variable2}`
+
+console.log(variable1);
+console.log(variable2);
+console.log(variable3);
